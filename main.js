c5a4cb1adce07fec500fed2d12976f7b32a4b118
jasmineavie <jasminemichaelavie@gmail.com>
2023-08-17 15:11:59 -0500

    8/17

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 const randomQuote = "http://api.quotable.io/random"
 const quoteDisplay = document.getElementById('quote')
 const quoteInputty = document.getElementById("userInput")
+const timerElement = document.getElementById("timer")
 
 quoteInputty.addEventListener("input", () => {
-console.log("changed")
+const arrayQuote = arrayquoteDisplayElement.querySelectorAll("span")
+const arrayValue = quoteInputElement.value.split('')
+
+
+let correct = true
+
+arrayQuote.forEach((characterSpan, index ) => {
+    const character = arrayValue[index]
+    if (character == null) {
+        characterSpan.classList.remove('correct')
+        characterSpan.classList.remove('incorrect')
+        correct = false
+    }
+    else if (character === characterSpan.innerText) {
+        characterSpan.classList.add('correct')
+        characterSpan.classList.remove('incorrect')
+    } else {
+        characterSpan.classList.remove('correct')
+        characterSpan.classList.add('incorrect')
+        correct = false
+    }
 })
+if(correct) getNextQuote()
+})
+
+
 
 function getQuote() {
     return fetch(randomQuote)
@@ -24,6 +49,21 @@ async function getNextQuote() {
         quoteDisplay.appendChild(characterSpan)
     })
     quoteInputty.value = null
+    startTimer()
+}
+let startTime
+function startTimer() {
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+       timerElement.innerText = getTimerTime()
+
+
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
 }
 
 getNextQuote()
